2f8d4591a2a51f88df20700ac0a106bcfff6472c
Sven Kulle <sven.kulle@est.fujitsu.com>
2017-11-16 11:35:49 +0100

    initial maven rest api operation with original sources

---
+++ oscm-rest-api-operation/src/main/java/org/oscm/rest/operation/SettingsResource.java
@@ -0,0 +1,74 @@
+package org.oscm.rest.operation;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.oscm.rest.common.CommonParams;
+import org.oscm.rest.common.RestResource;
+import org.oscm.rest.common.Since;
+import org.oscm.rest.operation.data.SettingRepresentation;
+
+@Path(CommonParams.PATH_VERSION + "/settings")
+@Stateless
+public class SettingsResource extends RestResource {
+
+    @EJB
+    SettingsBackend sb;
+
+    @Since(CommonParams.VERSION_1)
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSettings(@Context UriInfo uriInfo,
+            @BeanParam OperationParameters params) throws Exception {
+        return getCollection(uriInfo, sb.getCollection(), params);
+    }
+
+    @Since(CommonParams.VERSION_1)
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createSetting(@Context UriInfo uriInfo,
+            SettingRepresentation content, @BeanParam OperationParameters params)
+            throws Exception {
+        return post(uriInfo, sb.post(), content, params);
+    }
+
+    @Since(CommonParams.VERSION_1)
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path(CommonParams.PATH_ID)
+    public Response getSetting(@Context UriInfo uriInfo,
+            @BeanParam OperationParameters params) throws Exception {
+        return get(uriInfo, sb.get(), params, true);
+    }
+
+    @Since(CommonParams.VERSION_1)
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path(CommonParams.PATH_ID)
+    public Response updateUser(@Context UriInfo uriInfo,
+            SettingRepresentation content, @BeanParam OperationParameters params)
+            throws Exception {
+        return put(uriInfo, sb.put(), content, params);
+    }
+
+    @Since(CommonParams.VERSION_1)
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path(CommonParams.PATH_ID)
+    public Response deleteSetting(@Context UriInfo uriInfo,
+            @BeanParam OperationParameters params) throws Exception {
+        return delete(uriInfo, sb.delete(), params);
+    }
+
+}
