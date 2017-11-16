8590adf86f4e15e12d16ebf0309f0f001cb372f5
Sven Kulle <sven.kulle@est.fujitsu.com>
2017-11-16 13:18:51 +0100

    deleted old projects

--- oscm-rest-api-marketplace-old/javasrc/org/oscm/rest/marketplace/MarketplaceListType.java
+++
@@ -1,6 +0,0 @@
-package org.oscm.rest.marketplace;
-
-public enum MarketplaceListType {
-
-    OWNED, ALL, PUBLISH, ACCESSIBLE
-}
--- oscm-rest-api-service-old/javasrc/org/oscm/rest/service/data/ServiceStatus.java
+++
@@ -1,6 +0,0 @@
-package org.oscm.rest.service.data;
-
-public enum ServiceStatus {
-
-    ACTIVE, INACTIVE, RESUMED, SUSPENDED
-}
