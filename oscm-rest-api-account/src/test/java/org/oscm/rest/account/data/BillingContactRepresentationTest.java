9e7e775f77812a969bece25a5421e6bd783530da
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-04-10 22:49:52 +0200

    Added unit tests for Account API

---
+++ oscm-rest-api-account/src/test/java/org/oscm/rest/account/data/BillingContactRepresentationTest.java
@@ -0,0 +1,96 @@
+package org.oscm.rest.account.data;
+
+import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.VOBillingContact;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BillingContactRepresentationTest {
+
+  @Test
+  public void shouldUpdateVOBillingContact() {
+    BillingContactRepresentation representation = createBillingContactRepresentation();
+    VOBillingContact billingContact = null;
+
+    representation.update();
+    billingContact = representation.getVO();
+
+    assertThat(billingContact).isNotNull();
+    assertThat(billingContact)
+        .extracting(VOBillingContact::getAddress)
+        .isEqualTo(representation.getAddress());
+    assertThat(billingContact)
+        .extracting(VOBillingContact::getCompanyName)
+        .isEqualTo(representation.getCompanyName());
+    assertThat(billingContact)
+        .extracting(VOBillingContact::getId)
+        .isEqualTo(representation.getContactId());
+    assertThat(billingContact)
+        .extracting(VOBillingContact::getEmail)
+        .isEqualTo(representation.getEmail());
+    assertThat(billingContact)
+        .extracting(VOBillingContact::getKey)
+        .isEqualTo(representation.getId().longValue());
+    assertThat(billingContact)
+        .extracting(VOBillingContact::isOrgAddressUsed)
+        .isEqualTo(representation.isOrgAddressUsed());
+    assertThat(billingContact)
+        .extracting(VOBillingContact::getVersion)
+        .isEqualTo(representation.getETag().intValue());
+  }
+
+  @Test
+  public void shouldConvertToBillingContact() {
+    VOBillingContact voBillingContact = createVOBillingContact();
+
+    BillingContactRepresentation contactRepresentation =
+        new BillingContactRepresentation(voBillingContact);
+    contactRepresentation.convert();
+
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::getAddress)
+        .isEqualTo(voBillingContact.getAddress());
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::getCompanyName)
+        .isEqualTo(voBillingContact.getCompanyName());
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::getContactId)
+        .isEqualTo(voBillingContact.getId());
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::getEmail)
+        .isEqualTo(voBillingContact.getEmail());
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::getId)
+        .isEqualTo((long) voBillingContact.getKey());
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::isOrgAddressUsed)
+        .isEqualTo(voBillingContact.isOrgAddressUsed());
+    assertThat(contactRepresentation)
+        .extracting(BillingContactRepresentation::getETag)
+        .isEqualTo((long) voBillingContact.getVersion());
+  }
+
+  private BillingContactRepresentation createBillingContactRepresentation() {
+    BillingContactRepresentation contactRepresentation = new BillingContactRepresentation();
+    contactRepresentation.setAddress("address");
+    contactRepresentation.setCompanyName("companyName");
+    contactRepresentation.setContactId("contactId");
+    contactRepresentation.setEmail("email");
+    contactRepresentation.setId(123L);
+    contactRepresentation.setOrgAddressUsed(true);
+    contactRepresentation.setETag(456L);
+    return contactRepresentation;
+  }
+
+  private VOBillingContact createVOBillingContact() {
+    VOBillingContact billingContact = new VOBillingContact();
+    billingContact.setAddress("address");
+    billingContact.setCompanyName("companyName");
+    billingContact.setId("contactId");
+    billingContact.setEmail("email");
+    billingContact.setKey(123);
+    billingContact.setOrgAddressUsed(true);
+    billingContact.setVersion(456);
+    return billingContact;
+  }
+}
---
+++ oscm-rest-api-account/src/test/java/org/oscm/rest/account/data/PaymentInfoRepresentationTest.java
@@ -0,0 +1,79 @@
+package org.oscm.rest.account.data;
+
+import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.VOPaymentInfo;
+import org.oscm.internal.vo.VOPaymentType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PaymentInfoRepresentationTest {
+
+  @Test
+  public void shouldUpdateVOPaymentInfo() {
+    PaymentInfoRepresentation representation = createPaymentInfoRepresentation();
+    VOPaymentInfo paymentInfo = null;
+
+    representation.update();
+    paymentInfo = representation.getVO();
+
+    assertThat(paymentInfo).isNotNull();
+    assertThat(paymentInfo)
+        .extracting(VOPaymentInfo::getAccountNumber)
+        .isEqualTo(representation.getAccountNumber());
+    assertThat(paymentInfo).extracting(VOPaymentInfo::getId).isEqualTo(representation.getInfoId());
+    assertThat(paymentInfo).extracting(VOPaymentInfo::getKey).isEqualTo(representation.getId());
+    assertThat(paymentInfo)
+        .extracting(VOPaymentInfo::getPaymentType)
+        .isEqualTo(representation.getVO().getPaymentType());
+    assertThat(paymentInfo)
+        .extracting(VOPaymentInfo::getProviderName)
+        .isEqualTo(representation.getProviderName());
+    assertThat(paymentInfo)
+        .extracting(VOPaymentInfo::getVersion)
+        .isEqualTo(representation.getETag().intValue());
+  }
+
+  @Test
+  public void shouldConvertToPaymentRepresentation() {
+    VOPaymentInfo voPaymentInfo = createVoPaymentInfo();
+    PaymentInfoRepresentation representation = new PaymentInfoRepresentation(voPaymentInfo);
+
+    representation.convert();
+
+    assertThat(representation)
+        .extracting(PaymentInfoRepresentation::getAccountNumber)
+        .isEqualTo(voPaymentInfo.getAccountNumber());
+    assertThat(representation)
+        .extracting(PaymentInfoRepresentation::getId)
+        .isEqualTo(voPaymentInfo.getKey());
+    assertThat(representation)
+        .extracting(PaymentInfoRepresentation::getInfoId)
+        .isEqualTo(voPaymentInfo.getId());
+    assertThat(representation)
+        .extracting(PaymentInfoRepresentation::getProviderName)
+        .isEqualTo(voPaymentInfo.getProviderName());
+    assertThat(representation)
+        .extracting(PaymentInfoRepresentation::getETag)
+        .isEqualTo((long) voPaymentInfo.getVersion());
+  }
+
+  private PaymentInfoRepresentation createPaymentInfoRepresentation() {
+    PaymentInfoRepresentation representation = new PaymentInfoRepresentation();
+    representation.setAccountNumber("accountNr");
+    representation.setId(123L);
+    representation.setInfoId("infoId");
+    representation.setProviderName("providerName");
+    representation.setETag(456L);
+    return representation;
+  }
+
+  private VOPaymentInfo createVoPaymentInfo() {
+    VOPaymentInfo paymentInfo = new VOPaymentInfo();
+    paymentInfo.setAccountNumber("accountNr");
+    paymentInfo.setId("id");
+    paymentInfo.setPaymentType(new VOPaymentType());
+    paymentInfo.setProviderName("providerName");
+    paymentInfo.setVersion(456);
+    return paymentInfo;
+  }
+}
