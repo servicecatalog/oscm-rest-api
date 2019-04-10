2335fcf5c7e5f6dbc7b02ad050bb400cea784228
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-04-10 22:49:52 +0200

    Added unit tests for Account API

---
+++ oscm-rest-api-account/src/test/java/org/oscm/rest/account/AccountBackendTest.java
@@ -0,0 +1,67 @@
+package org.oscm.rest.account;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.oscm.internal.intf.AccountService;
+import org.oscm.internal.intf.OperatorService;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountBackendTest {
+  // TODO: These tests should be in fact integration tests
+
+  @Mock private AccountService accountService;
+  @Mock private OperatorService operatorService;
+  @InjectMocks private AccountBackend backend;
+
+  @BeforeEach
+  public void setUp() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldPostBillingContact() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldGetBillingContactCollection() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldGetBillingContact() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldPutBillingContact() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldDeleteBillingContact() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldGetPaymentInfoCollection() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldPutPaymentInfo() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldGetPaymentInfo() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldDeletePaymentInfo() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldPostOrganization() {}
+
+  @Test
+  @Disabled("Not implemented")
+  public void shouldGetOrganization() {}
+}
