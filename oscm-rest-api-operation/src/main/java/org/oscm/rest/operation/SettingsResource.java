1f1a984d2cacee837ece08311831d955855ef603
Travis CI <travis@travis-ci.org>
2019-04-15 11:57:40 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-operation/src/main/java/org/oscm/rest/operation/SettingsResource.java
+++ oscm-rest-api-operation/src/main/java/org/oscm/rest/operation/SettingsResource.java
@@ -51,7 +51,7 @@ public class SettingsResource extends RestResource {
   @Produces(MediaType.APPLICATION_JSON)
   @Path(CommonParams.PATH_ID)
   public Response getSetting(@Context UriInfo uriInfo, @BeanParam OperationParameters params)
-          throws Exception {
+      throws Exception {
     return get(uriInfo, sb.get(), params, true);
   }
 
--- oscm-rest-api-operation/src/test/java/org/oscm/rest/operation/OperationParametersTest.java
+++ oscm-rest-api-operation/src/test/java/org/oscm/rest/operation/OperationParametersTest.java
@@ -1,20 +1,26 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 15-04-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.operation;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class OperationParametersTest {
 
-    @Test
-    @Disabled("Not implemented yet")
-    void shouldValidateParametersSuccess() {
-    }
+  @Test
+  @Disabled("Not implemented yet")
+  void shouldValidateParametersSuccess() {}
 
-    @Test
-    @Disabled("Not implemented yet")
-    void shouldUpdateSuccess() {
-    }
-
+  @Test
+  @Disabled("Not implemented yet")
+  void shouldUpdateSuccess() {}
 }
