d54f0f9ba67633c2f5e95605fef8400d4ca53d17
Travis CI <travis@travis-ci.org>
2019-04-16 11:31:12 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-operation/src/test/java/org/oscm/rest/operation/SettingsBackendTest.java
+++ oscm-rest-api-operation/src/test/java/org/oscm/rest/operation/SettingsBackendTest.java
@@ -1,3 +1,12 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 16-04-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.operation;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,33 +18,30 @@ import org.oscm.internal.intf.OperatorService;
 
 class SettingsBackendTest {
 
-    @Mock
-    ConfigurationService cs;
+  @Mock ConfigurationService cs;
 
-    @Mock
-    OperatorService os;
+  @Mock OperatorService os;
 
-    @BeforeEach
-    public void setUp() {}
+  @BeforeEach
+  public void setUp() {}
 
-    @Test
-    @Disabled("Not implemented")
-    public void delete() {}
+  @Test
+  @Disabled("Not implemented")
+  public void delete() {}
 
-    @Test
-    @Disabled("Not implemented")
-    public void post() {}
+  @Test
+  @Disabled("Not implemented")
+  public void post() {}
 
-    @Test
-    @Disabled("Not implemented")
-    public void put() {}
+  @Test
+  @Disabled("Not implemented")
+  public void put() {}
 
-    @Test
-    @Disabled("Not implemented")
-    public void get() {}
+  @Test
+  @Disabled("Not implemented")
+  public void get() {}
 
-    @Test
-    @Disabled("Not implemented")
-    public void getCollection() {}
-
+  @Test
+  @Disabled("Not implemented")
+  public void getCollection() {}
 }
