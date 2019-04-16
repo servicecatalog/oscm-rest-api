1c698e5ffeb8cd83627fdfa18ca5fb4a53d0dfee
Travis CI <travis@travis-ci.org>
2019-04-16 11:31:35 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-operation/src/test/java/org/oscm/rest/operation/config/OperationResourceConfigTest.java
+++ oscm-rest-api-operation/src/test/java/org/oscm/rest/operation/config/OperationResourceConfigTest.java
@@ -1,28 +1,35 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 16-04-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.operation.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.oscm.rest.common.GsonMessageProvider;
 import org.oscm.rest.common.OSCMExceptionMapper;
 import org.oscm.rest.common.VersionFilter;
 import org.oscm.rest.operation.SettingsResource;
 
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class OperationResourceConfigTest {
 
-    @Test
-    public void shouldGetClasses() {
-        OperationResourceConfig resourceConfig = new OperationResourceConfig();
+  @Test
+  public void shouldGetClasses() {
+    OperationResourceConfig resourceConfig = new OperationResourceConfig();
 
-        Set<Class<?>> classes = resourceConfig.getClasses();
+    Set<Class<?>> classes = resourceConfig.getClasses();
 
-        assertThat(classes.size()).isEqualTo(4);
-        assertThat(classes.contains(SettingsResource.class)).isEqualTo(true);
-        assertThat(classes.contains(OSCMExceptionMapper.class)).isEqualTo(true);
-        assertThat(classes.contains(GsonMessageProvider.class)).isEqualTo(true);
-        assertThat(classes.contains(VersionFilter.class)).isEqualTo(true);
-    }
-
+    assertThat(classes.size()).isEqualTo(4);
+    assertThat(classes.contains(SettingsResource.class)).isEqualTo(true);
+    assertThat(classes.contains(OSCMExceptionMapper.class)).isEqualTo(true);
+    assertThat(classes.contains(GsonMessageProvider.class)).isEqualTo(true);
+    assertThat(classes.contains(VersionFilter.class)).isEqualTo(true);
+  }
 }
