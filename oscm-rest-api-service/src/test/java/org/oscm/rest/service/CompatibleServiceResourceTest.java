8499d363d4cc705f5b3169bfa26dfb677594d3a7
Travis CI <travis@travis-ci.org>
2019-04-18 12:53:26 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-service/src/test/java/org/oscm/rest/service/CompatibleServiceResourceTest.java
+++ oscm-rest-api-service/src/test/java/org/oscm/rest/service/CompatibleServiceResourceTest.java
@@ -1,6 +1,23 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 18-04-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.when;
+
 import com.google.common.collect.Lists;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import javax.ws.rs.core.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,202 +29,187 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.oscm.rest.common.RepresentationCollection;
 import org.oscm.rest.service.data.ServiceRepresentation;
 
-import javax.ws.rs.core.*;
-import java.net.URI;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 class CompatibleServiceResourceTest {
 
-    @Mock
-    private ServiceBackend serviceBackend;
+  @Mock private ServiceBackend serviceBackend;
 
-    @InjectMocks
-    @Spy
-    private CompatibleServiceResource compatibleServiceResource;
+  @InjectMocks @Spy private CompatibleServiceResource compatibleServiceResource;
 
-    private Response response;
-    private ServiceRepresentation serviceRepresentation;
-    private UriInfo uriInfo;
-    private ServiceParameters serviceParameters;
+  private Response response;
+  private ServiceRepresentation serviceRepresentation;
+  private UriInfo uriInfo;
+  private ServiceParameters serviceParameters;
 
-    @BeforeEach
-    public void setUp() {
-        serviceRepresentation = new ServiceRepresentation();
-        uriInfo = createUriInfo();
-        serviceParameters = createParameters();
+  @BeforeEach
+  public void setUp() {
+    serviceRepresentation = new ServiceRepresentation();
+    uriInfo = createUriInfo();
+    serviceParameters = createParameters();
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    response = null;
+  }
+
+  @Test
+  public void shouldGetCompatibleServices() {
+    when(serviceBackend.getCompatibles())
+        .thenReturn(
+            serviceParameters ->
+                new RepresentationCollection<>(Lists.newArrayList(serviceRepresentation)));
+
+    try {
+      response = compatibleServiceResource.getCompatibleServices(uriInfo, serviceParameters);
+    } catch (Exception e) {
+      fail(e);
     }
 
-    @AfterEach
-    public void cleanUp() {
-        response = null;
+    assertThat(response).isNotNull();
+    assertThat(response)
+        .extracting(Response::getStatus)
+        .isEqualTo(Response.Status.OK.getStatusCode());
+    assertThat(response).extracting(Response::hasEntity).isEqualTo(true);
+    assertThat(response)
+        .extracting(
+            r -> {
+              RepresentationCollection<ServiceRepresentation> representationCollection =
+                  (RepresentationCollection<ServiceRepresentation>) r.getEntity();
+              return representationCollection.getItems().toArray()[0];
+            })
+        .isEqualTo(serviceRepresentation);
+  }
 
+  @Test
+  public void shouldSetCompatibleServices() {
+    when(serviceBackend.putCompatibles())
+        .thenReturn((serviceRepresentation, serviceParameters) -> true);
+
+    try {
+      response =
+          compatibleServiceResource.setCompatibleServices(uriInfo, getContent(), serviceParameters);
+    } catch (Exception e) {
+      fail(e);
     }
 
-    @Test
-    public void shouldGetCompatibleServices() {
-        when(serviceBackend.getCompatibles())
-                .thenReturn(
-                        serviceParameters ->
-                                new RepresentationCollection<>(
-                                        Lists.newArrayList(serviceRepresentation)));
+    assertThat(response).isNotNull();
+    assertThat(response)
+        .extracting(Response::getStatus)
+        .isEqualTo(Response.Status.NO_CONTENT.getStatusCode());
+    assertThat(response).extracting(Response::hasEntity).isEqualTo(false);
+  }
 
-        try {
-            response = compatibleServiceResource.getCompatibleServices(uriInfo, serviceParameters);
-        } catch (Exception e) {
-            fail(e);
-        }
+  private RepresentationCollection<ServiceRepresentation> getContent() {
+    return new RepresentationCollection<>(Lists.newArrayList(serviceRepresentation));
+  }
 
-        assertThat(response).isNotNull();
-        assertThat(response)
-                .extracting(Response::getStatus)
-                .isEqualTo(Response.Status.OK.getStatusCode());
-        assertThat(response)
-                .extracting(Response::hasEntity).isEqualTo(true);
-        assertThat(response)
-                .extracting(
-                        r -> {
-                            RepresentationCollection<ServiceRepresentation> representationCollection =
-                                    (RepresentationCollection<ServiceRepresentation>) r.getEntity();
-                            return representationCollection.getItems().toArray()[0];
-                        })
-                .isEqualTo(serviceRepresentation);
-    }
+  private UriInfo createUriInfo() {
+    return new UriInfo() {
+      @Override
+      public String getPath() {
+        return null;
+      }
 
-    @Test
-    public void shouldSetCompatibleServices() {
-        when(serviceBackend.putCompatibles())
-                .thenReturn((serviceRepresentation, serviceParameters) -> true);
+      @Override
+      public String getPath(boolean b) {
+        return null;
+      }
 
-        try {
-            response = compatibleServiceResource.setCompatibleServices(uriInfo, getContent(), serviceParameters);
-        } catch (Exception e) {
-            fail(e);
-        }
+      @Override
+      public List<PathSegment> getPathSegments() {
+        return null;
+      }
 
-        assertThat(response).isNotNull();
-        assertThat(response)
-                .extracting(Response::getStatus)
-                .isEqualTo(Response.Status.NO_CONTENT.getStatusCode());
-        assertThat(response)
-                .extracting(Response::hasEntity).isEqualTo(false);
-    }
+      @Override
+      public List<PathSegment> getPathSegments(boolean b) {
+        return null;
+      }
 
-    private RepresentationCollection<ServiceRepresentation> getContent() {
-        return new RepresentationCollection<>(Lists.newArrayList(serviceRepresentation));
-    }
+      @Override
+      public URI getRequestUri() {
+        return null;
+      }
 
-    private UriInfo createUriInfo() {
-        return new UriInfo() {
-            @Override
-            public String getPath() {
-                return null;
-            }
+      @Override
+      public UriBuilder getRequestUriBuilder() {
+        return null;
+      }
 
-            @Override
-            public String getPath(boolean b) {
-                return null;
-            }
+      @Override
+      public URI getAbsolutePath() {
+        return null;
+      }
 
-            @Override
-            public List<PathSegment> getPathSegments() {
-                return null;
-            }
+      @Override
+      public UriBuilder getAbsolutePathBuilder() {
+        return null;
+      }
 
-            @Override
-            public List<PathSegment> getPathSegments(boolean b) {
-                return null;
-            }
+      @Override
+      public URI getBaseUri() {
+        return null;
+      }
 
-            @Override
-            public URI getRequestUri() {
-                return null;
-            }
+      @Override
+      public UriBuilder getBaseUriBuilder() {
+        return null;
+      }
 
-            @Override
-            public UriBuilder getRequestUriBuilder() {
-                return null;
-            }
-
-            @Override
-            public URI getAbsolutePath() {
-                return null;
-            }
-
-            @Override
-            public UriBuilder getAbsolutePathBuilder() {
-                return null;
-            }
-
-            @Override
-            public URI getBaseUri() {
-                return null;
-            }
-
-            @Override
-            public UriBuilder getBaseUriBuilder() {
-                return null;
-            }
-
-            @Override
-            public MultivaluedMap<String, String> getPathParameters() {
-                return new MultivaluedHashMap<String, String>() {
-                    {
-                        put("version", Collections.singletonList("v1"));
-                    }
-                };
-            }
-
-            @Override
-            public MultivaluedMap<String, String> getPathParameters(boolean b) {
-                return null;
-            }
-
-            @Override
-            public MultivaluedMap<String, String> getQueryParameters() {
-                return null;
-            }
-
-            @Override
-            public MultivaluedMap<String, String> getQueryParameters(boolean b) {
-                return null;
-            }
-
-            @Override
-            public List<String> getMatchedURIs() {
-                return null;
-            }
-
-            @Override
-            public List<String> getMatchedURIs(boolean b) {
-                return null;
-            }
-
-            @Override
-            public List<Object> getMatchedResources() {
-                return null;
-            }
-
-            @Override
-            public URI resolve(URI uri) {
-                return null;
-            }
-
-            @Override
-            public URI relativize(URI uri) {
-                return null;
-            }
+      @Override
+      public MultivaluedMap<String, String> getPathParameters() {
+        return new MultivaluedHashMap<String, String>() {
+          {
+            put("version", Collections.singletonList("v1"));
+          }
         };
-    }
+      }
 
-    private ServiceParameters createParameters() {
-        ServiceParameters parameters = new ServiceParameters();
-        parameters.setId(100L);
-        return parameters;
-    }
+      @Override
+      public MultivaluedMap<String, String> getPathParameters(boolean b) {
+        return null;
+      }
+
+      @Override
+      public MultivaluedMap<String, String> getQueryParameters() {
+        return null;
+      }
+
+      @Override
+      public MultivaluedMap<String, String> getQueryParameters(boolean b) {
+        return null;
+      }
+
+      @Override
+      public List<String> getMatchedURIs() {
+        return null;
+      }
+
+      @Override
+      public List<String> getMatchedURIs(boolean b) {
+        return null;
+      }
+
+      @Override
+      public List<Object> getMatchedResources() {
+        return null;
+      }
+
+      @Override
+      public URI resolve(URI uri) {
+        return null;
+      }
+
+      @Override
+      public URI relativize(URI uri) {
+        return null;
+      }
+    };
+  }
+
+  private ServiceParameters createParameters() {
+    ServiceParameters parameters = new ServiceParameters();
+    parameters.setId(100L);
+    return parameters;
+  }
 }
--- oscm-rest-api-service/src/test/java/org/oscm/rest/service/config/ServiceResourceConfigTest.java
+++ oscm-rest-api-service/src/test/java/org/oscm/rest/service/config/ServiceResourceConfigTest.java
@@ -1,31 +1,39 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 18-04-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.service.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.oscm.rest.common.GsonMessageProvider;
 import org.oscm.rest.common.OSCMExceptionMapper;
 import org.oscm.rest.common.VersionFilter;
 import org.oscm.rest.service.*;
 
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class ServiceResourceConfigTest {
 
-    @Test
-    public void shouldGetClasses() {
-        ServiceResourceConfig resourceConfig = new ServiceResourceConfig();
+  @Test
+  public void shouldGetClasses() {
+    ServiceResourceConfig resourceConfig = new ServiceResourceConfig();
 
-        Set<Class<?>> classes = resourceConfig.getClasses();
+    Set<Class<?>> classes = resourceConfig.getClasses();
 
-        assertThat(classes.size()).isEqualTo(4);
-        assertThat(classes.contains(CompatibleServiceResource.class)).isEqualTo(true);
-        assertThat(classes.contains(PriceModelResource.class)).isEqualTo(true);
-        assertThat(classes.contains(ServiceImageResource.class)).isEqualTo(true);
-        assertThat(classes.contains(TechnicalServiceResource.class)).isEqualTo(true);
-        assertThat(classes.contains(TSSupplierResource.class)).isEqualTo(true);
-        assertThat(classes.contains(OSCMExceptionMapper.class)).isEqualTo(true);
-        assertThat(classes.contains(GsonMessageProvider.class)).isEqualTo(true);
-        assertThat(classes.contains(VersionFilter.class)).isEqualTo(true);
-    }
+    assertThat(classes.size()).isEqualTo(4);
+    assertThat(classes.contains(CompatibleServiceResource.class)).isEqualTo(true);
+    assertThat(classes.contains(PriceModelResource.class)).isEqualTo(true);
+    assertThat(classes.contains(ServiceImageResource.class)).isEqualTo(true);
+    assertThat(classes.contains(TechnicalServiceResource.class)).isEqualTo(true);
+    assertThat(classes.contains(TSSupplierResource.class)).isEqualTo(true);
+    assertThat(classes.contains(OSCMExceptionMapper.class)).isEqualTo(true);
+    assertThat(classes.contains(GsonMessageProvider.class)).isEqualTo(true);
+    assertThat(classes.contains(VersionFilter.class)).isEqualTo(true);
+  }
 }
