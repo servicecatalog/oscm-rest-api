d40af5648628f0d840aeafbace1c238da82abf13
Travis CI <travis@travis-ci.org>
2019-04-24 11:24:05 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/Dummyclass5.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/Dummyclass5.java
@@ -1,4 +1,12 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 24-04-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.account;
 
-public class Dummyclass5 {
-}
+public class Dummyclass5 {}
