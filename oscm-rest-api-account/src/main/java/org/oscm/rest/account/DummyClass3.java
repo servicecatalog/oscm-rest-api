3ddf01efe8f918c69653a9693237b54ea8f6994e
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-04-24 13:00:48 +0200

    Dummy commit

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/DummyClass3.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/DummyClass3.java
@@ -1,12 +1,14 @@
-/**
- * *****************************************************************************
- *
- * <p>Copyright FUJITSU LIMITED 2019
- *
- * <p>Creation Date: 24-04-2019
- *
- * <p>*****************************************************************************
- */
 package org.oscm.rest.account;
 
-public class DummyClass3 {}
+public class DummyClass3 {
+
+
+        public void test() {
+
+
+
+
+
+                
+        }
+}
