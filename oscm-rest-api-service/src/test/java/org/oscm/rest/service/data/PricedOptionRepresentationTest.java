2228ffcabc4d9f990dca3a1106087a8980e5b410
Lukasz Wiecek <lukasz.wiecek@ts.fujitsu.com>
2019-04-25 08:37:05 +0200

    add more unit tests

--- oscm-rest-api-service/src/test/java/org/oscm/rest/service/data/PricedOptionRepresentationTest.java
+++ oscm-rest-api-service/src/test/java/org/oscm/rest/service/data/PricedOptionRepresentationTest.java
@@ -16,7 +16,11 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.BaseVO;
 import org.oscm.internal.vo.VOPricedOption;
+import org.oscm.internal.vo.VOPricedRole;
+import org.oscm.internal.vo.VORoleDefinition;
+import org.oscm.rest.common.Representation;
 
 class PricedOptionRepresentationTest {
 
@@ -39,5 +43,108 @@ class PricedOptionRepresentationTest {
     VOPricedOption result = representation.getVO();
 
     assertThat(result).isNotNull();
+    assertThat(result)
+            .extracting(BaseVO::getKey)
+            .isEqualTo(representation.convertIdToKey());
+      assertThat(result)
+              .extracting(VOPricedOption::getOptionId)
+              .isEqualTo(representation.getOptionId());
+      assertThat(result)
+              .extracting(VOPricedOption::getParameterOptionKey)
+              .isEqualTo(representation.getParameterOptionKey());
+      assertThat(result)
+              .extracting(VOPricedOption::getPricePerSubscription)
+              .isEqualTo(representation.getPricePerSubscription());
+      assertThat(result)
+              .extracting(VOPricedOption::getPricePerUser)
+              .isEqualTo(representation.getPricePerUser());
+      assertThat(
+              ((VOPricedRole)result.getRoleSpecificUserPrices().toArray()[0]).getPricePerUser())
+              .isEqualTo(pricedRoleRepresentation.getPricePerUser());
+      assertThat(result)
+              .extracting(BaseVO::getVersion)
+              .isEqualTo(representation.convertETagToVersion());
   }
+
+    @Test
+    public void shouldUpdateVOPricedOption_evenIfIdAndETagIsNull() {
+        PricedOptionRepresentation representation = new PricedOptionRepresentation();
+        representation.setOptionId("100");
+        representation.setParameterOptionKey(100L);
+        representation.setPricePerSubscription(BigDecimal.TEN);
+        representation.setPricePerUser(BigDecimal.TEN);
+        List<PricedRoleRepresentation> list = new ArrayList<>();
+        PricedRoleRepresentation pricedRoleRepresentation = new PricedRoleRepresentation();
+        pricedRoleRepresentation.setPricePerUser(BigDecimal.ONE);
+        list.add(pricedRoleRepresentation);
+        representation.setRoleSpecificUserPrices(list);
+
+        representation.update();
+        VOPricedOption result = representation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result)
+                .extracting(BaseVO::getKey)
+                .isEqualTo(representation.convertIdToKey());
+        assertThat(result)
+                .extracting(VOPricedOption::getOptionId)
+                .isEqualTo(representation.getOptionId());
+        assertThat(result)
+                .extracting(VOPricedOption::getParameterOptionKey)
+                .isEqualTo(representation.getParameterOptionKey());
+        assertThat(result)
+                .extracting(VOPricedOption::getPricePerSubscription)
+                .isEqualTo(representation.getPricePerSubscription());
+        assertThat(result)
+                .extracting(VOPricedOption::getPricePerUser)
+                .isEqualTo(representation.getPricePerUser());
+        assertThat(
+                ((VOPricedRole)result.getRoleSpecificUserPrices().toArray()[0]).getPricePerUser())
+                .isEqualTo(pricedRoleRepresentation.getPricePerUser());
+        assertThat(result)
+                .extracting(BaseVO::getVersion)
+                .isEqualTo(representation.convertETagToVersion());
+    }
+
+    @Test
+    public void shouldConvertToPricedOptionRepresentation() {
+      VOPricedOption voPricedOption = new VOPricedOption();
+      voPricedOption.setKey(100L);
+      voPricedOption.setOptionId("Option100");
+      voPricedOption.setParameterOptionKey(100L);
+      voPricedOption.setPricePerSubscription(BigDecimal.TEN);
+      voPricedOption.setPricePerUser(BigDecimal.TEN);
+      List<VOPricedRole> list = new ArrayList<>();
+      VOPricedRole voPricedRole = new VOPricedRole();
+      voPricedRole.setPricePerUser(BigDecimal.ZERO);
+        VORoleDefinition voRoleDefinition = new VORoleDefinition();
+      voPricedRole.setRole(voRoleDefinition);
+      list.add(voPricedRole);
+      voPricedOption.setRoleSpecificUserPrices(list);
+      voPricedOption.setVersion(100);
+
+      PricedOptionRepresentation representation = new PricedOptionRepresentation(voPricedOption);
+      representation.convert();
+
+        assertThat(representation)
+                .extracting(PricedOptionRepresentation::convertETagToVersion)
+                .isEqualTo(voPricedOption.getVersion());
+        assertThat(representation)
+              .extracting(Representation::getId)
+              .isEqualTo(voPricedOption.getKey());
+        assertThat(representation)
+                .extracting(PricedOptionRepresentation::getOptionId)
+                .isEqualTo(voPricedOption.getOptionId());
+        assertThat(representation)
+                .extracting(PricedOptionRepresentation::getParameterOptionKey)
+                .isEqualTo(voPricedOption.getParameterOptionKey());
+        assertThat(representation)
+                .extracting(PricedOptionRepresentation::getPricePerSubscription)
+                .isEqualTo(voPricedOption.getPricePerSubscription());
+        assertThat(representation)
+                .extracting(PricedOptionRepresentation::getPricePerUser)
+                .isEqualTo(voPricedOption.getPricePerUser());
+        assertThat(((PricedRoleRepresentation)representation.getRoleSpecificUserPrices().toArray()[0]).getPricePerUser())
+                .isEqualTo(voPricedRole.getPricePerUser());
+    }
 }
