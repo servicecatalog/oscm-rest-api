d6a41439d20bc4c39c5f1dc5b80756b3c9c9dfba
Lukasz <lukaszwiecek92@gmail.com>
2019-05-27 10:29:57 +0200

    Implement unit tests for subscription API

--- oscm-rest-api-subscription/src/test/java/org/oscm/rest/subscription/data/BillingContactRepresentationTest.java
+++ oscm-rest-api-subscription/src/test/java/org/oscm/rest/subscription/data/BillingContactRepresentationTest.java
@@ -1,7 +1,65 @@
 package org.oscm.rest.subscription.data;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.BaseVO;
+import org.oscm.internal.vo.VOBillingContact;
+import org.oscm.rest.common.Representation;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BillingContactRepresentationTest {
+
+    @Test
+    public void shouldUpdateVOBillingContact() {
+        BillingContactRepresentation billingContactRepresentation = createRepresentation();
+        billingContactRepresentation.setETag(100L);
+        billingContactRepresentation.setId(100L);
+
+        billingContactRepresentation.update();
+        VOBillingContact result = billingContactRepresentation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result).extracting(BaseVO::getKey).isEqualTo(result.getKey());
+        assertThat(result).extracting(BaseVO::getVersion).isEqualTo(result.getVersion());
+    }
+
+    @Test
+    public void shouldUpdateVOBillingContact_evenIfIdandETagIsNull() {
+        BillingContactRepresentation billingContactRepresentation = createRepresentation();
+
+        billingContactRepresentation.update();
+        VOBillingContact result = billingContactRepresentation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result).extracting(BaseVO::getKey).isEqualTo(result.getKey());
+        assertThat(result).extracting(BaseVO::getVersion).isEqualTo(result.getVersion());
+    }
+
+    @Test
+    public void shouldConvertToBillingContact() {
+        VOBillingContact voBillingContact = createVO();
+
+        BillingContactRepresentation representation = new BillingContactRepresentation(voBillingContact);
+        representation.convert();
+
+        assertThat(representation)
+                .extracting(Representation::getId)
+                .isEqualTo(voBillingContact.getKey());
+        assertThat(representation)
+                .extracting(Representation::getETag)
+                .isEqualTo((long) voBillingContact.getVersion());
+    }
+
+    private VOBillingContact createVO() {
+        VOBillingContact voBillingContact = new VOBillingContact();
+        voBillingContact.setId("100");
+        voBillingContact.setVersion(100);
+        return voBillingContact;
+    }
+
+    private BillingContactRepresentation createRepresentation() {
+        BillingContactRepresentation billingContactRepresentation = new BillingContactRepresentation();
+        return billingContactRepresentation;
+    }
 
 }
--- oscm-rest-api-subscription/src/test/java/org/oscm/rest/subscription/data/ParameterDefinitionRepresentationTest.java
+++ oscm-rest-api-subscription/src/test/java/org/oscm/rest/subscription/data/ParameterDefinitionRepresentationTest.java
@@ -1,7 +1,64 @@
 package org.oscm.rest.subscription.data;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.BaseVO;
+import org.oscm.internal.vo.VOParameterDefinition;
+import org.oscm.rest.common.Representation;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ParameterDefinitionRepresentationTest {
 
+    @Test
+    public void shouldUpdateVOParameterDefinition() {
+        ParameterDefinitionRepresentation parameterDefinitionRepresentation = createRepresentation();
+        parameterDefinitionRepresentation.setETag(100L);
+        parameterDefinitionRepresentation.setId(100L);
+
+        parameterDefinitionRepresentation.update();
+        VOParameterDefinition result = parameterDefinitionRepresentation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result).extracting(BaseVO::getKey).isEqualTo(result.getKey());
+        assertThat(result).extracting(BaseVO::getVersion).isEqualTo(result.getVersion());
+    }
+
+    @Test
+    public void shouldUpdateVOBillingContact_evenIfIdandETagIsNull() {
+        ParameterDefinitionRepresentation parameterDefinitionRepresentation = createRepresentation();
+
+        parameterDefinitionRepresentation.update();
+        VOParameterDefinition result = parameterDefinitionRepresentation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result).extracting(BaseVO::getKey).isEqualTo(result.getKey());
+        assertThat(result).extracting(BaseVO::getVersion).isEqualTo(result.getVersion());
+    }
+
+    @Test
+    public void shouldConvertToBillingContact() {
+        VOParameterDefinition voParameterDefinition = createVO();
+
+        ParameterDefinitionRepresentation representation = new ParameterDefinitionRepresentation(voParameterDefinition);
+        representation.convert();
+
+        assertThat(representation)
+                .extracting(Representation::getId)
+                .isEqualTo(voParameterDefinition.getKey());
+        assertThat(representation)
+                .extracting(Representation::getETag)
+                .isEqualTo((long) voParameterDefinition.getVersion());
+    }
+
+    private VOParameterDefinition createVO() {
+        VOParameterDefinition voParameterDefinition = new VOParameterDefinition();
+        voParameterDefinition.setKey(100L);
+        voParameterDefinition.setVersion(100);
+        return voParameterDefinition;
+    }
+
+    private ParameterDefinitionRepresentation createRepresentation() {
+        ParameterDefinitionRepresentation parameterDefinitionRepresentation = new ParameterDefinitionRepresentation();
+        return parameterDefinitionRepresentation;
+    }
 }
