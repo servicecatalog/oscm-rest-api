d0be478a0feb9b58cc9e9d79bdd1bf60c35b06b7
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-07-09 07:44:50 +0200

    Moved representation tests to commons package

---
+++ oscm-rest-api-common/src/test/java/org/oscm/rest/common/representation/PricedRoleRepresentationTest.java
@@ -0,0 +1,98 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 25-04-2019
+ *
+ * <p>*****************************************************************************
+ */
+package org.oscm.rest.common.representation;
+
+import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.BaseVO;
+import org.oscm.internal.vo.VOPricedRole;
+import org.oscm.internal.vo.VORoleDefinition;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PricedRoleRepresentationTest {
+
+  @Test
+  public void shouldUpdateVOPricedRole() {
+    PricedRoleRepresentation representation = createRepresentation();
+    representation.setId(100L);
+    representation.setETag(100L);
+
+    representation.update();
+    VOPricedRole result = representation.getVO();
+
+    assertThat(result).isNotNull();
+    assertThat(result).extracting(BaseVO::getKey).isEqualTo(representation.convertIdToKey());
+    assertThat(result)
+        .extracting(VOPricedRole::getPricePerUser)
+        .isEqualTo(representation.getPricePerUser());
+    assertThat(result.getRole().getRoleId()).isEqualTo(representation.getRole().getRoleId());
+    assertThat(result)
+        .extracting(BaseVO::getVersion)
+        .isEqualTo(representation.convertETagToVersion());
+  }
+
+  @Test
+  public void shouldUpdateVOPricedRole_evenIfIdAndETagIsNull() {
+    PricedRoleRepresentation representation = createRepresentation();
+
+    representation.update();
+    VOPricedRole result = representation.getVO();
+
+    assertThat(result).isNotNull();
+    assertThat(result).extracting(BaseVO::getKey).isEqualTo(representation.convertIdToKey());
+    assertThat(result)
+        .extracting(VOPricedRole::getPricePerUser)
+        .isEqualTo(representation.getPricePerUser());
+    assertThat(result.getRole().getRoleId()).isEqualTo(representation.getRole().getRoleId());
+    assertThat(result)
+        .extracting(BaseVO::getVersion)
+        .isEqualTo(representation.convertETagToVersion());
+  }
+
+  @Test
+  public void shouldConvertToPricedRoleRepresentation() {
+    VOPricedRole voPricedRole = createVO();
+
+    PricedRoleRepresentation representation = new PricedRoleRepresentation(voPricedRole);
+    representation.convert();
+
+    assertThat(representation)
+        .extracting(PricedRoleRepresentation::convertETagToVersion)
+        .isEqualTo(voPricedRole.getVersion());
+    assertThat(representation).extracting(Representation::getId).isEqualTo(voPricedRole.getKey());
+    assertThat(representation)
+        .extracting(PricedRoleRepresentation::getPricePerUser)
+        .isEqualTo(voPricedRole.getPricePerUser());
+    assertThat(representation.getRole().getDescription())
+        .isEqualTo(voPricedRole.getRole().getDescription());
+  }
+
+  private PricedRoleRepresentation createRepresentation() {
+    PricedRoleRepresentation representation = new PricedRoleRepresentation();
+    representation.setPricePerUser(BigDecimal.TEN);
+    RoleDefinitionRepresentation roleDefinitionRepresentation = new RoleDefinitionRepresentation();
+    roleDefinitionRepresentation.setRoleId("Role100");
+    representation.setRole(roleDefinitionRepresentation);
+    return representation;
+  }
+
+  private VOPricedRole createVO() {
+    VOPricedRole voPricedRole = new VOPricedRole();
+    voPricedRole.setVersion(100);
+    voPricedRole.setKey(100L);
+    voPricedRole.setPricePerUser(BigDecimal.TEN);
+    VORoleDefinition voRoleDefinition = new VORoleDefinition();
+    voRoleDefinition.setDescription("Description100");
+    voPricedRole.setRole(voRoleDefinition);
+    return voPricedRole;
+  }
+}
---
+++ oscm-rest-api-common/src/test/java/org/oscm/rest/common/representation/ServiceRepresentationTest.java
@@ -0,0 +1,64 @@
+package org.oscm.rest.common.representation;
+
+import org.junit.jupiter.api.Test;
+import org.oscm.internal.vo.BaseVO;
+import org.oscm.internal.vo.VOService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ServiceRepresentationTest {
+
+    @Test
+    public void shouldUpdateVOService() {
+        ServiceRepresentation serviceRepresentation = createRepresentation();
+        serviceRepresentation.setETag(100L);
+        serviceRepresentation.setId(100L);
+
+        serviceRepresentation.update();
+        VOService result = serviceRepresentation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result).extracting(BaseVO::getKey).isEqualTo(result.getKey());
+        assertThat(result).extracting(BaseVO::getVersion).isEqualTo(result.getVersion());
+    }
+
+    @Test
+    public void shouldUpdateVOService_evenIfIdAndETagIsNull() {
+        ServiceRepresentation serviceRepresentation = createRepresentation();
+
+        serviceRepresentation.update();
+        VOService result = serviceRepresentation.getVO();
+
+        assertThat(result).isNotNull();
+        assertThat(result).extracting(BaseVO::getKey).isEqualTo(result.getKey());
+        assertThat(result).extracting(BaseVO::getVersion).isEqualTo(result.getVersion());
+    }
+
+    @Test
+    public void shouldConvertToServiceRepresentation() {
+        VOService voService = createVO();
+
+        ServiceRepresentation representation = new ServiceRepresentation(voService);
+        representation.convert();
+
+        assertThat(representation)
+                .extracting(ServiceRepresentation::getServiceId)
+                .isEqualTo(voService.getServiceId());
+        assertThat(representation)
+                .extracting(Representation::getETag)
+                .isEqualTo((long) voService.getVersion());
+    }
+
+    private VOService createVO() {
+        VOService voService = new VOService();
+        voService.setServiceId("100ID");
+        voService.setVersion(100);
+        return voService;
+    }
+
+    private ServiceRepresentation createRepresentation() {
+        ServiceRepresentation serviceRepresentation = new ServiceRepresentation();
+        return serviceRepresentation;
+    }
+
+}
