fd44dd971c0f13dd046b0e5d54902321506be203
OSCM Automaton <oscm.automaton@gmail.com>
2019-08-01 11:00:01 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/SomeNewClass.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/SomeNewClass.java
@@ -1,16 +1,24 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: 01-08-2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.account;
 
 public class SomeNewClass {
 
+  public void testtttmtm() {
 
-        public void testtttmtm() {
-
-        System.out.println();
     System.out.println();
-                System.out.println();
     System.out.println();
-                                System.out.println();
     System.out.println();
-                        System.out.println();
-        }
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+  }
 }
