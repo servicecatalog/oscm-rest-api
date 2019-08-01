420caee40ad9829654f0e19b16c5db34f7f77ef1
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-08-01 13:38:42 +0200

    WIP: Fixing formatter

---
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/SomeNewClass22.java
@@ -0,0 +1,17 @@
+package org.oscm.rest.account;
+
+public class SomeNewClass22                      {
+
+
+
+
+
+                        public void teeest() {
+    System.out.println();
+System.out.println();
+                    System.out.println();
+                                System.out.println();
+                                                System.out.println();
+                System.out.println()        ;
+        }
+                        }
