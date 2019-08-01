c8b359bb37e7aa4bb5dd1de0894f54af1e3226de
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-08-01 16:09:10 +0200

    WIP: Fixing formatter

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/SomeNewClass.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/SomeNewClass.java
@@ -1,24 +1,16 @@
-/**
- * *****************************************************************************
- *
- * <p>Copyright FUJITSU LIMITED 2019
- *
- * <p>Creation Date: 01-08-2019
- *
- * <p>*****************************************************************************
- */
+
 package org.oscm.rest.account;
 
 public class SomeNewClass {
 
   public void testtttmtm() {
 
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
+                            System.out.println();
+                        System.out.println();
+                System.out.println();
+        System.out.println();
+System.out.println();
+                    System.out.println();
     System.out.println();
   }
 }
