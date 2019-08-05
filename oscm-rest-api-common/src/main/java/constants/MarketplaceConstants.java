2dbeeaed607882e0dbafc5fede7492800cf3830c
Lukasz Wiecek <lukasz.wiecek@ts.fujitsu.com>
2019-08-05 13:41:17 +0200

    Add marketplace api swagger documentation

---
+++ oscm-rest-api-common/src/main/java/constants/MarketplaceConstants.java
@@ -0,0 +1,27 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: Aug 05, 2019
+ *
+ * <p>*****************************************************************************
+ */
+
+package constants;
+
+/**
+ * Class for marketplace api module constants
+ *
+ * @author crystalzord
+ */
+
+public class MarketplaceConstants {
+    public static final String ENTRY_MINIMUM_BODY = "";
+
+    public static final String ENTRY_MAXIMUM_BODY = "";
+
+    public static final String MARKETPLACE_MINIMUM_BODY = "";
+
+    public static final String MARKETPLACE_MAXIMUM_BODY = "";
+}
--- oscm-rest-api-marketplace/src/main/java/org/oscm/rest/marketplace/MarketplaceResource.java
+++ oscm-rest-api-marketplace/src/main/java/org/oscm/rest/marketplace/MarketplaceResource.java
@@ -9,6 +9,14 @@
  */
 package org.oscm.rest.marketplace;
 
+import constants.CommonConstants;
+import constants.MarketplaceConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AccessLevel;
 import lombok.Setter;
 import org.oscm.rest.common.CommonParams;
@@ -33,6 +41,17 @@ public class MarketplaceResource extends RestResource {
   MarketplaceBackend mb;
 
   @GET
+  @Operation(summary = "Get all marketplaces",
+          tags = {"marketplace"},
+          description = "Returns all marketplaces",
+          responses = {
+                  @ApiResponse(responseCode = "200",
+                          description = "Marketplace list",
+                          content = @Content(
+                                  mediaType = "application/json",
+                                  schema = @Schema(implementation = MarketplaceRepresentation.class)
+                          ))
+          })
   public Response getMarketplaces(@Context UriInfo uriInfo, @BeanParam MarketplaceParameters params)
       throws Exception {
     return getCollection(uriInfo, mb.getCollection(), params);
@@ -40,12 +59,42 @@ public class MarketplaceResource extends RestResource {
 
   @GET
   @Path(CommonParams.PATH_ID)
+  @Operation(summary = "Get a single marketplace",
+          tags = {"marketplace"},
+          description = "Returns a single marketplace",
+          responses = {
+                  @ApiResponse(responseCode = "200", description = "A single marketplace", content = @Content(
+                          schema = @Schema(implementation = MarketplaceRepresentation.class)
+                  ))
+          })
   public Response getMarketplace(@Context UriInfo uriInfo, @BeanParam MarketplaceParameters params)
           throws Exception {
     return get(uriInfo, mb.get(), params, true);
   }
 
   @POST
+  @Operation(summary = "Create a marketplace",
+          tags = {"marketplace"},
+          description = "Creates a marketplace",
+          requestBody = @RequestBody(
+                  description = "MarketplaceRepresentation object to be created",
+                  required = true,
+                  content = @Content(
+                          schema = @Schema(implementation = MarketplaceRepresentation.class),
+                          examples = {
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MINIMUM_BODY_NAME,
+                                          value= MarketplaceConstants.MARKETPLACE_MINIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MINIMUM_BODY_SUMMARY),
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MAXIMUM_BODY_NAME,
+                                          value= MarketplaceConstants.MARKETPLACE_MAXIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MAXIMUM_BODY_SUMMARY)
+                          }
+                  )),
+          responses = {
+                  @ApiResponse(responseCode = "201", description = "Marketplace created successfully")
+          })
   public Response createMarketplace(
       @Context UriInfo uriInfo,
       MarketplaceRepresentation content,
@@ -56,6 +105,27 @@ public class MarketplaceResource extends RestResource {
 
   @PUT
   @Path(CommonParams.PATH_ID)
+  @Operation(summary = "Update a single marketplace",
+          tags = {"marketplace"},
+          description = "Updates a single marketplace",
+          requestBody = @RequestBody(
+                  description = "MarketplaceRepresentation object to be updated",
+                  required = true,
+                  content = @Content(
+                          schema = @Schema(implementation = MarketplaceRepresentation.class),
+                          examples = {
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MINIMUM_BODY_NAME,
+                                          value= MarketplaceConstants.MARKETPLACE_MINIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MINIMUM_BODY_SUMMARY),
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MAXIMUM_BODY_NAME,
+                                          value= MarketplaceConstants.MARKETPLACE_MAXIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MAXIMUM_BODY_SUMMARY)
+                          })),
+          responses = {
+                  @ApiResponse(responseCode = "204", description = "Billing contact updated successfully")
+          })
   public Response updateMarketplace(
       @Context UriInfo uriInfo,
       MarketplaceRepresentation content,
@@ -66,6 +136,12 @@ public class MarketplaceResource extends RestResource {
 
   @DELETE
   @Path(CommonParams.PATH_ID)
+  @Operation(summary = "Delete a single marketplace",
+          tags = {"marketplace"},
+          description = "Deletes a single marketplace",
+          responses = {
+                  @ApiResponse(responseCode = "204", description = "Marketplace deleted successfully")
+          })
   public Response deleteMarketplace(
       @Context UriInfo uriInfo, @BeanParam MarketplaceParameters params) throws Exception {
     return delete(uriInfo, mb.delete(), params);
