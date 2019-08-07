146393a3578a5db10d102402172c1b6ebaf7b960
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-08-07 17:04:56 +0200

    Documented Service API with Swagger

--- oscm-rest-api-service/src/main/java/org/oscm/rest/service/CompatibleServiceResource.java
+++ oscm-rest-api-service/src/main/java/org/oscm/rest/service/CompatibleServiceResource.java
@@ -9,6 +9,13 @@
  */
 package org.oscm.rest.service;
 
+import constants.ServiceConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AccessLevel;
 import lombok.Setter;
 import org.oscm.rest.common.CommonParams;
@@ -38,12 +45,44 @@ public class CompatibleServiceResource extends RestResource {
 
 
   @GET
+  @Operation(summary = "Get all services compatible to selected service",
+          tags = {"services"},
+          description = "Get all services compatible to selected service",
+          responses = {
+                  @ApiResponse(responseCode = "200",
+                          description = "Services list",
+                          content = @Content(
+                                  mediaType = "application/json",
+                                  schema = @Schema(implementation = ServiceRepresentation.class)
+                          ))
+          })
   public Response getCompatibleServices(
       @Context UriInfo uriInfo, @BeanParam ServiceParameters params) throws Exception {
     return getCollection(uriInfo, sb.getCompatibles(), params);
   }
 
   @PUT
+  @Operation(summary = "Update a single billing contact",
+          tags = {"billingcontacts"},
+          description = "Updates a single billing contact",
+          requestBody = @RequestBody(
+                  description = "BillingContactRepresentation object to be updated",
+                  required = true,
+                  content = @Content(
+                          schema = @Schema(implementation = ServiceRepresentation.class),
+                          examples = {
+                                  @ExampleObject(
+                                          name = "Only required parameters",
+                                          value= ServiceConstants.COMPATIBLE_SERVICE_MINIMUM_BODY,
+                                          summary = "Minimum body example"),
+                                  @ExampleObject(
+                                          name = "All possible parameters",
+                                          value= ServiceConstants.COMPATIBLE_SERVICE_MAXIMUM_BODY,
+                                          summary = "Maximum body example")
+                          })),
+          responses = {
+                  @ApiResponse(responseCode = "204", description = "Billing contact updated successfully")
+          })
   public Response setCompatibleServices(
       @Context UriInfo uriInfo,
       RepresentationCollection<ServiceRepresentation> content,
--- oscm-rest-api-service/src/main/java/org/oscm/rest/service/PriceModelResource.java
+++ oscm-rest-api-service/src/main/java/org/oscm/rest/service/PriceModelResource.java
@@ -9,6 +9,15 @@
  */
 package org.oscm.rest.service;
 
+import constants.AccountConstants;
+import constants.CommonConstants;
+import constants.ServiceConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AccessLevel;
 import lombok.Setter;
 import org.oscm.rest.common.CommonParams;
@@ -36,29 +45,90 @@ public class PriceModelResource extends RestResource {
   PriceModelBackend pmb;
 
   @GET
+  @Operation(summary = "Get generic price model for the service",
+          tags = {"services"},
+          description = "Returns price model for the service",
+          responses = {
+                  @ApiResponse(responseCode = "200",
+                          description = "Price model item",
+                          content = @Content(
+                                  mediaType = "application/json",
+                                  schema = @Schema(implementation = PriceModelRepresentation.class)
+                          ))
+          })
   public Response get(@Context UriInfo uriInfo, @BeanParam ServiceParameters params)
       throws Exception {
     return get(uriInfo, pmb.get(), params, true);
   }
 
+  @PUT
+  @Operation(summary = "Update generic price model for the service",
+          tags = {"services"},
+          description = "Updates generic price model for the service",
+          requestBody = @RequestBody(
+                  description = "PriceModelRepresentation object to be updated",
+                  required = true,
+                  content = @Content(
+                          schema = @Schema(implementation = PriceModelRepresentation.class),
+                          examples = {
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MINIMUM_BODY_NAME,
+                                          value= ServiceConstants.SERVICE_PRICE_MODEL_MINIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MINIMUM_BODY_SUMMARY),
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MAXIMUM_BODY_NAME,
+                                          value= ServiceConstants.SERVICE_PRICE_MODEL_MAXIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MAXIMUM_BODY_SUMMARY)
+                          })),
+          responses = {
+                  @ApiResponse(responseCode = "204", description = "Price model updated successfully")
+          })
+  public Response update(
+          @Context UriInfo uriInfo,
+          PriceModelRepresentation content,
+          @BeanParam ServiceParameters params)
+          throws Exception {
+    return put(uriInfo, pmb.put(), content, params);
+  }
+
   @GET
   @Path("/customer/{orgKey}")
+  @Operation(summary = "Get customer-specific price model for service",
+          tags = {"services"},
+          description = "Returns price model for the service",
+          responses = {
+                  @ApiResponse(responseCode = "200", description = "Price model for the service", content = @Content(
+                          schema = @Schema(implementation = PriceModelRepresentation.class)
+                  ))
+          })
   public Response getForCustomer(@Context UriInfo uriInfo, @BeanParam ServiceParameters params)
           throws Exception {
     return get(uriInfo, pmb.getForCustomer(), params, true);
   }
 
   @PUT
-  public Response update(
-      @Context UriInfo uriInfo,
-      PriceModelRepresentation content,
-      @BeanParam ServiceParameters params)
-      throws Exception {
-    return put(uriInfo, pmb.put(), content, params);
-  }
-
-  @PUT
   @Path("/customer/{orgKey}")
+  @Operation(summary = "Update customer-specific price model for service",
+          tags = {"services"},
+          description = "Updates customer-specific price model for service",
+          requestBody = @RequestBody(
+                  description = "PriceModelRepresentation object to be updated",
+                  required = true,
+                  content = @Content(
+                          schema = @Schema(implementation = PriceModelRepresentation.class),
+                          examples = {
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MINIMUM_BODY_NAME,
+                                          value= ServiceConstants.CUSTOMER_PRICE_MODEL_MINIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MINIMUM_BODY_SUMMARY),
+                                  @ExampleObject(
+                                          name = CommonConstants.EXAMPLE_MAXIMUM_BODY_NAME,
+                                          value= ServiceConstants.CUSTOMER_PRICE_MODEL_MAXIMUM_BODY,
+                                          summary = CommonConstants.EXAMPLE_MAXIMUM_BODY_SUMMARY)
+                          })),
+          responses = {
+                  @ApiResponse(responseCode = "204", description = "Price model updated successfully")
+          })
   public Response updateForCustomer(
       @Context UriInfo uriInfo,
       PriceModelRepresentation content,
