ab87b0b9e221fee4334eea26b2db76214f9096f1
OSCM Automaton <oscm.automaton@gmail.com>
2020-03-26 09:04:34 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/OrganizationResource.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/OrganizationResource.java
@@ -76,7 +76,8 @@ public class OrganizationResource extends RestResource {
   @Operation(
       summary = "Creates organization",
       tags = {"organizations"},
-      description = "Creates an organization along with its administrator based on given request data",
+      description =
+          "Creates an organization along with its administrator based on given request data",
       requestBody =
           @RequestBody(
               description = "JSON representing organization to be created",
