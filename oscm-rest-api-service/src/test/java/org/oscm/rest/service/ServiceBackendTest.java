40269b209a8e2c9b6059434fe420ec5ef6ffafd1
cworf91 <christian.worf@est.fujitsu.com>
2020-03-31 11:46:17 +0200

    Add unit tests

--- oscm-rest-api-service/src/test/java/org/oscm/rest/service/ServiceBackendTest.java
+++ oscm-rest-api-service/src/test/java/org/oscm/rest/service/ServiceBackendTest.java
@@ -1,7 +1,17 @@
 package org.oscm.rest.service;
 
-import com.google.common.collect.Lists;
-import lombok.SneakyThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,25 +20,30 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.oscm.internal.intf.SearchService;
 import org.oscm.internal.intf.ServiceProvisioningService;
+import org.oscm.internal.types.exception.InvalidPhraseException;
+import org.oscm.internal.types.exception.ObjectNotFoundException;
+import org.oscm.internal.vo.VOService;
 import org.oscm.internal.vo.VOServiceDetails;
-import org.oscm.internal.vo.VOTechnicalService;
-import org.oscm.rest.common.ServiceStatus;
-import org.oscm.rest.common.representation.*;
+import org.oscm.internal.vo.VOServiceListResult;
 import org.oscm.rest.common.SampleTestDataUtility;
+import org.oscm.rest.common.ServiceStatus;
+import org.oscm.rest.common.representation.RepresentationCollection;
+import org.oscm.rest.common.representation.ServiceDetailsRepresentation;
+import org.oscm.rest.common.representation.ServiceRepresentation;
+import org.oscm.rest.common.representation.StatusRepresentation;
 import org.oscm.rest.common.requestparameters.ServiceParameters;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import com.google.common.collect.Lists;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import lombok.SneakyThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class ServiceBackendTest {
 
   @Mock private ServiceProvisioningService service;
+  @Mock private SearchService searchService;
   @InjectMocks private ServiceBackend backend;
   private ServiceResource resource;
   private CompatibleServiceResource compatiblesResource;
@@ -70,6 +85,30 @@ public class ServiceBackendTest {
   @SneakyThrows
   public void shouldGetServices() {
     when(service.getSuppliedServices()).thenReturn(Lists.newArrayList(vo));
+
+    Response response = resource.getServices(uriInfo, parameters);
+
+    assertThat(response).isNotNull();
+    assertThat(response)
+        .extracting(Response::getStatus)
+        .isEqualTo(Response.Status.OK.getStatusCode());
+    assertThat(response)
+        .extracting(
+            r -> {
+              return ((RepresentationCollection) r.getEntity()).getItems().size();
+            })
+        .isEqualTo(1);
+  }
+
+  @Test
+  @SneakyThrows
+  public void shouldGetServicesForPhrase() {
+
+    // given
+    mockSearchService();
+    parameters.setLanguage("en");
+    parameters.setServiceName("testService");
+    parameters.setMarketPlaceId("123456789");
 
     Response response = resource.getServices(uriInfo, parameters);
 
@@ -169,8 +208,56 @@ public class ServiceBackendTest {
         .isEqualTo(Response.Status.NO_CONTENT.getStatusCode());
   }
 
+  @Test
+  public void shouldGetdefaultLocal() {
+    // given
+    String expected = "en";
+
+    // when
+    String result = backend.getLocale("");
+
+    // then
+    assertThat(expected).isEqualTo(result);
+  }
+
+  @Test
+  public void shouldReturnGivenLocal() {
+    // given
+    String expected = "de";
+
+    // when
+    String result = backend.getLocale("de");
+
+    // then
+    assertThat(expected).isEqualTo(result);
+  }
+
+  public void shouldReturnService() throws ObjectNotFoundException, InvalidPhraseException {
+    // given
+    mockSearchService();
+
+    // when
+    List<VOService> slr = backend.getServices("12345678", "en", "test");
+
+    // then
+    assertThat(slr.get(0).getServiceId()).isEqualTo("testId");
+  }
+
+  private void mockSearchService() throws ObjectNotFoundException, InvalidPhraseException {
+    List<VOService> slr = new ArrayList<VOService>();
+    VOServiceListResult VOslr = new VOServiceListResult();
+    VOService s = new VOService();
+
+    s.setServiceId("testId");
+    slr.add(s);
+    VOslr.setServices(slr);
+
+    when(searchService.searchServices(any(), any(), any())).thenReturn(VOslr);
+  }
+
   private RepresentationCollection<ServiceRepresentation> createCompatiblesCollection() {
-    ServiceRepresentation serviceRepresentation = SampleTestDataUtility.createServiceRepresentation();
+    ServiceRepresentation serviceRepresentation =
+        SampleTestDataUtility.createServiceRepresentation();
 
     RepresentationCollection<ServiceRepresentation> collection =
         new RepresentationCollection<>(Lists.newArrayList(serviceRepresentation));
