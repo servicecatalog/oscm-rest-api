5ef83d00faeaf04311042b4d47923f90ebdca50b
Lorenz Goebel <GoebelL@users.noreply.github.com>
2021-11-24 15:42:59 +0100

    Formatting test

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/Dummy.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/Dummy.java
@@ -1,17 +1,11 @@
-/**
- * *****************************************************************************
- *
- * <p>Copyright FUJITSU LIMITED 2021
- *
- * <p>Creation Date: 18-11-2021
- *
- * <p>*****************************************************************************
- */
+
 package org.oscm.rest.account;
 
-/** Just a formatting test! */
-public class Dummy {
-  private Dummy() {
-    // unformatted code
-  }
+      /** Just a formatting test! */
+  public class Dummy {
+           private Dummy()  {
+             
+  //  This should appear well formatted.         
+      
+            }
 }
