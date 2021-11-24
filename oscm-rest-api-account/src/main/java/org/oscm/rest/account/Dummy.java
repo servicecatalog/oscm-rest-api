5a23c667a1d7d6b8ac380137f8ca1e1afeda828d
OSCM Automaton <oscm.automaton@gmail.com>
2021-11-24 23:21:16 +0000

    Applied code formatting [ci skip]

--- oscm-rest-api-account/src/main/java/org/oscm/rest/account/Dummy.java
+++ oscm-rest-api-account/src/main/java/org/oscm/rest/account/Dummy.java
@@ -1,13 +1,19 @@
-
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2021
+ *
+ * <p>Creation Date: 24-11-2021
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.rest.account;
 
-      /**   Just a formatting test! */
-  public class Dummy {
-              private Dummy()  {
+/** Just a formatting test! */
+public class Dummy {
+  private Dummy() {
 
+    //    This should appear well formatted.
 
-
-         //    This should appear well formatted.
-
-                                }
   }
+}
